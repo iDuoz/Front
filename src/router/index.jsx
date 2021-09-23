c0ba10d78d64eb6961460b2289d210eef89c250f
euncherry <csmo2642@naver.com>
2021-09-24 07:45:51 +0900

    :fire: remove check API code

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,41 +1,8 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import React from "react"
-import { useEffect } from "react"
-
-
-/**
- * @description 할당된 모든 구역 받아오기
- * @method GET
- * @request @headers YAT token
- * @request @body data{title , content , region , nor , dov , tov , dod}
- */
-const checkEmailValidation = () => {
-    return fetch('https://us-central1-eun-cherry.cloudfunctions.net/api/smileun', {
-        method: 'GET',
-        headers: {
-
-        }
-    }).then((res) => {
-        if (res.status === 500) throw Promise.resolve({ errorCode: 500, errorName: "Server error" })
-        if (!res.ok) throw res.json()
-        return res.json()
-    }).then(data => {
-        return data
-    }).catch(async (error) => {
-        let err = await error.then()
-        console.log("Error from check_email_validation\n" + err.errorCode + "\n" + err.errorName)
-        //에러처리
-        throw err
-    })
-}
 
 const AppRouter = () => {
 
-    useEffect(() => {
-        checkEmailValidation()
-            .then(res => console.log(res))
-            .catch(error => console.log(error))
-    }, [])
     return (
         <>
             <BrowserRouter>
