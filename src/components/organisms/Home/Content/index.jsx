ce8aa1adea528ce792b4ea3bfdc3a156439f65ea
euncherry <csmo2642@naver.com>
2021-09-29 03:02:54 +0900

    :alembic: i wanna drag zIndex

--- src/components/organisms/Home/Content/index.jsx
+++ src/components/organisms/Home/Content/index.jsx
@@ -21,14 +21,15 @@ const HomeContent = ({ testRef, parallaxScroll, currentSlider, sectionScrollType
     return (
         <>
             {/* SECTION Content */}
-            {
-                (check > 0) ?
-                    <div style={{ position: 'relative', left: '50%', top: '50%', zIndex: '1000' }}>
-                        {check}'    '{checkout}</div> : null
-            }
+
             <div ref={testRef} onWheel={parallaxScroll} onDragEnd={handelDragDrop} onDragStart={handelDragEnter} style={{
-                position: 'relative', height: '100%', width: "100%"
+                position: 'relative', height: '100%', width: "100%", zIndex: '1000'
             }}>
+                {
+                    (check > 0) ?
+                        <div style={{ position: 'relative', width: "20px", height: '20px', left: '30%', top: '50%', zIndex: "11" }}>
+                            {check}'    '{checkout}</div> : null
+                }
                 <FirstSectionForm currentSlider={currentSlider} sectionScrollType={sectionScrollType[0]} settingMobileBtn={settingMobileBtn} ></FirstSectionForm>
                 <SecondSectionForm currentSlider={currentSlider} sectionScrollType={sectionScrollType[1]} settingMobileBtn={settingMobileBtn}></SecondSectionForm>
                 <ThirdSectionForm currentSlider={currentSlider} sectionScrollType={sectionScrollType[2]} settingMobileBtn={settingMobileBtn}></ThirdSectionForm>
