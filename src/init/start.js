804de62ac08f7c3487256bd2ea71796fa314357e
euncherry <csmo2642@naver.com>
2021-10-01 11:05:21 +0900

    :ambulance: hosting error start authedit

--- src/init/start.js
+++ src/init/start.js
@@ -4,23 +4,23 @@ import { getAuth } from 'firebase/auth';
 const start = () => {
   console.log('start');
 
-  const auth = getAuth();
-  const user = auth.currentUser;
+  //   const auth = getAuth();
+  //   const user = auth.currentUser;
 
-  if (!user) return console.log('login user x');
+  //   if (!user) return console.log('login user x');
 
-  console.log(user);
-  console.log(user.uid);
-  store.dispatch(ACTION.LOGIN_ACTION_FUNC());
-  store.dispatch(
-    ACTION.SET_USER__ACTION_FUNC({
-      user: {
-        userUID: user.uid,
-        email: user.email,
-        name: user.displayName,
-      },
-    })
-  );
+  //   console.log(user);
+  //   console.log(user.uid);
+  //   store.dispatch(ACTION.LOGIN_ACTION_FUNC());
+  //   store.dispatch(
+  //     ACTION.SET_USER__ACTION_FUNC({
+  //       user: {
+  //         userUID: user.uid,
+  //         email: user.email,
+  //         name: user.displayName,
+  //       },
+  //     })
+  //   );
 };
 
 export default start;
