d40937a14d17874633989635e97372d21ab82671
euncherry <csmo2642@naver.com>
2021-10-13 14:38:27 +0900

    :goal_net: fuck iphone

--- src/service/transaction/login_process.js
+++ src/service/transaction/login_process.js
@@ -1,6 +1,5 @@
 import store from '../../store/store';
 import ACTION from '../../store/actions/action';
-import user_reducer from '../../store/reducers/user_reducer';
 import firebase_login from '../firebase/auth/logIn_password';
 import getUserData from '../firebase/database/getUserData';
 import getRegionArray from '../firebase/database/getRegionArray';
@@ -8,75 +7,126 @@ import { notification } from 'antd';
 
 // import get_userInfo from "../api/get/get_userInfo"
 
-const LogInProcess = (logInInfo) => {
-  console.log('loginProcess');
-  console.log(logInInfo);
-  firebase_login(logInInfo)
-    .then((res) => {
-      store.dispatch(ACTION.LOGIN_ACTION_FUNC());
+// const LogInProcess = (logInInfo) => {
+//   console.log('loginProcess');
+//   console.log(logInInfo);
+//   firebase_login(logInInfo)
+//     .then((res) => {
+//       store.dispatch(ACTION.LOGIN_ACTION_FUNC());
 
-      console.log(res);
-      console.log(res.uid);
-      notification['success']({
-        message: '아이폰 로그인 리덕스 ㅈ저ㅏㅇ',
-        description: `리덕스 로그인이요`,
-      });
-      return res.uid;
-    })
-    .then((uid) => {
-      // TODO 유저정보 받아오기
-      console.log('로그인하고 정보 넣어보까용');
-      console.log(uid);
+//       console.log(res);
+//       console.log(res.uid);
+//       notification['success']({
+//         message: '아이폰 로그인 리덕스 ㅈ저ㅏㅇ',
+//         description: `리덕스 로그인이요`,
+//       });
+//       return res.uid;
+//     })
+//     .then((uid) => {
+//       // TODO 유저정보 받아오기
+//       console.log('로그인하고 정보 넣어보까용');
+//       console.log(uid);
 
-      getUserData(uid)
-        .then((res) => {
-          console.log('정보 받아오기완료 ,,');
-          console.log(res);
-          store.dispatch(
-            ACTION.SET_USER__ACTION_FUNC({
-              user: {
-                uid: uid,
-                basic: res.basic,
-                loveNotice: res.loveNotice,
-                merit: res.merit,
-                role: res.role,
-                totalLoveNotice: res.totalLoveNotice,
-              },
-            })
-          );
-          console.log('정보 받아오기완료?????????????????????????????????????????');
+//       getUserData(uid)
+//         .then((res) => {
+//           console.log('정보 받아오기완료 ,,');
+//           console.log(res);
+//           store.dispatch(
+//             ACTION.SET_USER__ACTION_FUNC({
+//               user: {
+//                 uid: uid,
+//                 basic: res.basic,
+//                 loveNotice: res.loveNotice,
+//                 merit: res.merit,
+//                 role: res.role,
+//                 totalLoveNotice: res.totalLoveNotice,
+//               },
+//             })
+//           );
+//           console.log('정보 받아오기완료?????????????????????????????????????????');
 
-          notification['success']({
-            message: '아이폰 리덕스 저장',
-            description: `아이폰 ㅗㅗㅗㅗ`,
-          });
-        })
-        .catch((e) => {
-          notification['error']({
-            message: `get userData 실패😥 `,
-            description: e.message || e.code,
-          });
-        });
-    })
-    .catch((e) => {
-      notification['error']({
-        message: `로그인 .catch 실패😥 `,
-        description: e.message || e.code,
-      });
+//           notification['success']({
+//             message: '아이폰 리덕스 저장',
+//             description: `아이폰 ㅗㅗㅗㅗ`,
+//           });
+//         })
+//         .catch((e) => {
+//           notification['error']({
+//             message: `get userData 실패😥 `,
+//             description: e.message || e.code,
+//           });
+//         });
+//     })
+//     .catch((e) => {
+//       notification['error']({
+//         message: `로그인 .catch 실패😥 `,
+//         description: e.message || e.code,
+//       });
+//     });
+
+//   getRegionArray()
+//     .then((res) => {
+//       console.log('region 정보 보여줌');
+//       console.log(res);
+//       store.dispatch(ACTION.SET_REGION__ACTION_FUNC(res));
+//     })
+//     .catch((e) => {
+//       notification['error']({
+//         message: `getRegionArray 실패😥 `,
+//         description: e.message || e.code,
+//       });
+//     });
+// };
+
+const LogInProcess = async (logInInfo) => {
+  try {
+    const setLogin = await firebase_login(logInInfo);
+
+    const userId = setLogin.uid;
+
+    console.log('userId' + userId);
+    const setUserInfo = await getUserData(userId);
+
+    const userInfo = setUserInfo;
+
+    store.dispatch(
+      ACTION.SET_USER__ACTION_FUNC({
+        user: {
+          uid: userId,
+          basic: userInfo.basic,
+          loveNotice: userInfo.loveNotice,
+          merit: userInfo.merit,
+          role: userInfo.role,
+          totalLoveNotice: userInfo.totalLoveNotice,
+        },
+      })
+    );
+    notification['info']({
+      message: '유저정보 리덕스 저장 성공',
+      description: `유저정보 리덕스 저장 성공`,
     });
 
-  getRegionArray()
-    .then((res) => {
-      console.log('region 정보 보여줌');
-      console.log(res);
-      store.dispatch(ACTION.SET_REGION__ACTION_FUNC(res));
-    })
-    .catch((e) => {
-      notification['error']({
-        message: `getRegionArray 실패😥 `,
-        description: e.message || e.code,
-      });
+    const setRegion = await getRegionArray();
+    const regionInfo = setRegion;
+
+    store.dispatch(ACTION.SET_REGION__ACTION_FUNC(regionInfo));
+    notification['info']({
+      message: '지역정보 리덕스 저장 성공',
+      description: `지역정보 리덕스 저장 성공`,
     });
+
+    store.dispatch(ACTION.LOGIN_ACTION_FUNC());
+
+    notification['info']({
+      message: '아이폰 로그인 리덕스 ㅈ저ㅏㅇ',
+      description: `리덕스 로그인이요`,
+    });
+  } catch (e) {
+    notification['info']({
+      message: '로그인 리덕스 ㅈ저ㅏㅇ 실패',
+      description: `리덕스 로그인이요 실패요`,
+    });
+  }
 };
 
 export default LogInProcess;
