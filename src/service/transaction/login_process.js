f16aba9a288c36f4a2896f6b4fd0f6603d4fd791
euncherry <csmo2642@naver.com>
2021-10-13 17:50:50 +0900

    :goal_net: userInfo  notification rererer

--- src/service/transaction/login_process.js
+++ src/service/transaction/login_process.js
@@ -83,14 +83,14 @@ const LogInProcess = async (logInInfo) => {
     const setLogin = await firebase_login(logInInfo);
 
     notification['info']({
-      message: '이거나올라나?',
+      message: '이거나올라나?❤❤❤🍒',
       description: `나오냐?`,
     });
     const userId = setLogin.uid;
 
     console.log('userId' + userId);
     notification['info']({
-      message: 'userId💛',
+      message: 'userId🧡🧡🧡🧡',
       description: `${userId}`,
     });
     // const setUserInfo = await getUserData(userId);
@@ -116,33 +116,36 @@ const LogInProcess = async (logInInfo) => {
     //     },
     //   })
     // );
-    await getUserData(userId)
-      .then((res) => {
-        notification['info']({
-          message: 'userInfo🧸🧸',
-          description: `유저정보받아와받아와`,
-        });
-        console.log(res);
-        store.dispatch(
-          ACTION.SET_USER__ACTION_FUNC({
-            user: {
-              uid: userId,
-              basic: res.basic,
-              loveNotice: res.loveNotice,
-              merit: res.merit,
-              role: res.role,
-              totalLoveNotice: res.totalLoveNotice,
-            },
-          })
-        );
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-    notification['info']({
-      message: '🎀🎀 곰돌이 다음 유저정보 리덕스 저장 성공🎀🎀',
-      description: `유저정보 리덕스 저장 성공`,
-    });
+    // await getUserData(userId)
+    //   .then((res) => {
+    //     notification['info']({
+    //       message: 'userInfo🧸🧸💛💛💛',
+    //       description: `유저정보받아와받아와`,
+    //     });
+    //     console.log(res);
+    //     store.dispatch(
+    //       ACTION.SET_USER__ACTION_FUNC({
+    //         user: {
+    //           uid: userId,
+    //           basic: res.basic,
+    //           loveNotice: res.loveNotice,
+    //           merit: res.merit,
+    //           role: res.role,
+    //           totalLoveNotice: res.totalLoveNotice,
+    //         },
+    //       })
+    //     );
+    //   })
+    //   .catch((e) => {
+    //     notification['info']({
+    //       message: 'userInfo❤😢❤',
+    //       description: `유저정보못받아옴`,
+    //     });
+    //   });
+    // notification['info']({
+    //   message: ' 곰돌이 다음 유저정보 리덕스 저장 성공.💚💚💚💚',
+    //   description: `유저정보 리덕스 저장 성공`,
+    // });
 
     // const setRegion = await getRegionArray();
     // const regionInfo = setRegion;
@@ -156,7 +159,7 @@ const LogInProcess = async (logInInfo) => {
     store.dispatch(ACTION.LOGIN_ACTION_FUNC());
 
     notification['info']({
-      message: '아이폰 로그인 리덕스 ㅈ저ㅏㅇ',
+      message: '아이폰 로그인 리덕스 ㅈ저ㅏㅇ1.💙💙💙💙',
       description: `리덕스 로그인이요`,
     });
   } catch (e) {
@@ -172,7 +175,7 @@ const LogInProcess = async (logInInfo) => {
 
     store.dispatch(ACTION.SET_REGION__ACTION_FUNC(regionInfo));
     notification['info']({
-      message: '지역정보 리덕스 저장 성공🎯🐉🐉🐉🤍',
+      message: '지역정보 리덕스 저장 성공💜💜💜💜💜',
       description: `지역정보 리덕스 저장 성공`,
     });
   } catch (e) {
