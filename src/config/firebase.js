2f660427e3988e3881d4e68ff0ace548f66abd50
euncherry <csmo2642@naver.com>
2021-10-17 05:10:19 +0900

    :ambulance: git action

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,10 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBngd4AMRSJR19dX5-rrPQGAFP6f0Jbt_o',
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASEURL,
-  projectId: 'eun-cherry',
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
   appId: process.env.REACT_APP_APP_ID,
